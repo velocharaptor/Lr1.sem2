0845326d25afc2494bf465bcf1a54f0039e00723
Pankrat <99604167+velocharaptor@users.noreply.github.com>
2022-10-28 19:07:21 +0300

    Add files via upload

---
+++ tests.h
@@ -0,0 +1,10 @@
+#ifndef LR2_TESTS_H
+#define LR2_TESTS_H
+
+//void StartTesting();
+//void ArrayTesting();
+//void ListTesting();
+//void StekTesting();
+
+string SortTests();
+#endif //LR2_TESTS_H
